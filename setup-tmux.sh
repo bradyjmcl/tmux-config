30e13f89032e2cf86913a147852eb30b44ccb9a3
Brady McLaughlin <78229080+bradyjmcl@users.noreply.github.com>
2024-11-01 12:24:54 -0400

    Update setup-tmux.sh
    Added catpuccin setup so that it no longer relies on TPM

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # copy conf file
-mkdir ~/.config/tmux
+mkdir -p ~/.config/tmux/plugins/catppuccin
 cp tmux.conf ~/.config/tmux/tmux.conf
+
+# clone catpuccin
+git clone -b v2.1.0 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
 
 # clone TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
